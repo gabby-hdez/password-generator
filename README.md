c7d77487cf5660fd64c94562fc56c979bde709ff
Gabby Hernandez <gabbyhdezz697@gmail.com>
2022-02-20 21:25:01 -0500

    almost done

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# password-generator
+##PASSWORD GENERATOR
+
+For Week 3 we were introduced to Java Script. For this homework assignemtn we were given starter code which included the Index.HTML, Style.CSS and part of the Script.JS. Our main focus was to complete the Java Script portion of the code.
+
+For this assignemtn we needed to use JavaScript to create a random password generator. There were certain requirements that we had to meet in order for the application to be successful which included:
+    ## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+To complete the accepted Criteria we had to follow certain steps in order to be successful.
+
+    1. Review the HTML code to see what terms you need to use to link your HTML and JS.
+    2. Once you are done reviewing HTML, you move on to your JS.
+    3. In your JS, first you set up your global variables for the lowercase, uppercase, numbers and special characters.
+    4. Then you create your window prompts and window confirms for the criteria you want your password to contain.
+    5. After that you create a n array pool that contains all the characters in the chosen character sets.
+    6. Then you create an array variable for password and initialize it to an empty array.
+    7. Next you create a For loop for the length of the password and in each iteration you create an random index from the array pool in Step 5.
+    8. Then you check to make sure we include at least one character from the charater type (set) the user chose to inclue in the password.
+    9. The last thing to do is to convert the password array to a password string and return the password string to the caller.
--- script.js
+++ script.js
@@ -1,51 +1,95 @@
 let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let uppercase = lowercase.join(" ").toUpperCase().split(" ");
+let uppercase = lowercase.map((x) => x.toUpperCase());
 let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
-let number = [1234567890];
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
 
 function generatePassword() {
-  let passwordStr;
+  let lengthOfpassword = prompt("The password length must be between 8 to 128 characters. Enter how many characters you want to use.");
 
-
-  return passwordStr;
-
-}
-
-function getLength() {
-  let passwordLength = window.prompt("The password length must be a minimum of 8 characters and a maximum of 128 characters!");
-  if(passwordLength.length >= 8 && passwordLength.length <=128) {
-    return true;
+  if (!lengthOfpassword) {
+    alert("Please input a number value!");
+  }
+  else if (lengthOfpassword < 8 || lengthOfpassword > 128) {
+    lengthOfpassword = prompt("Choose between 8 to 128!");
   }
   else {
-    window.alert("The password must be bewtween 8 to 128 characters!");
-    return getLength();
+  var getLower = confirm("Press OK to have Lowercase characters in password!");
+  var getUpper = confirm("Press OK to have Uppercase characters in password!");
+  var getSpecial = confirm("Press OK to have Special characters in password!");
+  var getNumbers = confirm("Press OK to have Number characters in password!");
+  };
+
+  if (!(getLower && getUpper && getSpecial && getNumbers)) {
+    var options = alert("You must select one type of character from the options given. \nPress Ok to try again, or cancel to Quit.");
+
   }
+
+  else if (getLower && getUpper && getSpecial && getNumbers) {
+    options = lowercase.concat(uppercase, specialChar, number);
+  }
+
+else if (getLower && getUpper && getSpecial) {
+  options = lowercase.concat(uppercase, specialChar);
 
 }
+else if (getLower && getUpper && getNumbers) {
+  options = lowercase.concat(uppercase, number);
 
-function getCharacterOptions() {
-  let getLower = confirm("Press OK to have Lowercase characters in password!");
-  let getUpper = confirm("Press OK to have Uppercase characters in password!");
-  let getSpecial = confirm("Press OK to have Special characters in password!")
-  let getNumbers = confirm("Press OK to have Number characters in password!");
+}
+else if (getLower && getNumbers && getSpecial) {
+  options = lowercase.concat(number, specialChar);
+}
+else if (getUpper && getSpecial && getNumbers) {
+  options= uppercase.concat(specialChar, number);
+}
+else if (getUpper && getLower) {
+  options = lowercase.concat(uppercase);
+}
+else if (getLower && getSpecial) {
+  options = lowercase.concat(specialChar); 
+}
+else if (getLower && getNumbers) {
+  options = lowercase.concat(number); 
+}
+else if (getUpper && getSpecial) {
+  options = uppercase.concat(specialChar);
+}
+else if (getUpper && getNumbers) {
+  options = uppercase.concat(number);
+}
+else if (getSpecial && getNumbers) {
+  options = specialChar.concat(number);
+}
+else if (getLower) {
+  options = lowercase;
+}
+else if (getUpper) {
+  options = uppercase;
+}
+else if (getSpecial) {
+  options = specialChar;
+}
+else if (getNumbers) {
+  options = number;
+};
 
-  if (!(getLower || getUpper || getSpecial || getNumbers)) {
-    let tryAgain = confirm("You must select one type of character from the options given. \nPress Ok to try again, or cancel to Quit.");
+var password = [];
 
-    if (tryAgain) {
-      getCharacterOptions()
-    }
-  }
-  return {
-    getLower:getLower,
-    getUpper:getUpper,
-    getSpecial:getSpecial,
-    getNumbers:getNumbers
-  }
+for (var i = 0; i < lengthOfpassword; i++) {
+  let randomOptions = options[Math.floor(Math.random() * options.length)];
+  password.push(randomOptions);
+}
 
+var passwordStr = password.join("");
+
+return passwordStr
 }
 
 
+function writePassword(passwordStr) {
+  document.getElementById("password").textContent = passwordStr
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
